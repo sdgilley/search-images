1e7ccc791057dc6f8343f10efa1ebc738ccd7dda
Sheri Gilley <sgilley@microsoft.com>
2022-10-06 11:15:12 -0500

    add alternate auth, modify print

--- ocr-quickstart.py
+++ ocr-quickstart.py
@@ -13,12 +13,19 @@ from PIL import Image
 import sys
 import time
 
+# *** AUTHENTICATE - use one of these two methods, comment out the other
+# Authenticate with key, endpoint from environment variables (assumes you've exported these)
+import os
+endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
+subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
+
+# Or uncomment and fill in your values endpoint and key from the Azure portal. 
 '''
-Authenticate
-Authenticates your credentials and creates a client.
-'''
+# Authenticate with key, endpoint from Cognitive Services
 subscription_key = "<ADD-YOUR-KEY>"
 endpoint = "<ADD-YOUR-ENDPOINT>"
+'''
+# *** End of Authenticate - you're now ready to run the script.
 
 computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
 '''
@@ -32,8 +39,8 @@ This API call can also extract handwriting style text (not shown).
 '''
 print("===== Read File - remote =====")
 # Get an image with text
-read_image_url = "https://github.com/MicrosoftDocs/azure-docs/blob/main/articles/machine-learning/media\how-to-create-labeling-projects\exported-dataset.png?raw=true"
 read_image_url = "https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/main/articles/machine-learning/media/how-to-create-labeling-projects/add-label.png"
+# read_image_url = "https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/main/articles/machine-learning/media/how-to-create-attach-studio/create-schedule.png"
 # Call API with URL and raw response (allows you to get the operation location)
 read_response = computervision_client.read(read_image_url,  raw=True)
 
@@ -54,7 +61,8 @@ if read_result.status == OperationStatusCodes.succeeded:
     for text_result in read_result.analyze_result.read_results:
         for line in text_result.lines:
             if (line.text.find("Datastores"))>= 0:
-                print(line.text)
+                print("*** Found ",line.text, "! ***")
+
 print()
 '''
 END - Read File - remote
